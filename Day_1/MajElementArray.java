6d32524a618653d1372822985292e9b64d0ea159
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-02-24 21:56:26 +0530

    Delete MajElementArray.java

--- Day_1/MajElementArray.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        
-        Arrays.sort(nums);
-        int mid = (nums.length) /2;
-        return nums[mid];
-        
-    }
-}
