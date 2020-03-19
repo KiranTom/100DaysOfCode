d0f307461ae5f66dd54c735f930a8b952fc684ed
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-03-19 10:37:03 +0530

    Add files via upload

---
+++ Day 34/DeleteNode.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        
+        ListNode n = node.next;
+        node.next = n.next;
+        node.val = n.val;
+    }
+}
