94ce0f1ad8173298d62373f098b8d5071f6ffc81
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-05-09 14:39:29 +0530

    Add files via upload

---
+++ Day 85/IsBalanced.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(helper(root) == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    public int helper(TreeNode x) {
+        
+        if (x == null) {
+            return 0;
+        }
+        int left = helper(x.left);
+        int right = helper(x.right);
+
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
