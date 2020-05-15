758cba3c61b416ac91b3bc16992cbb0deacbb348
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-05-15 13:44:25 +0530

    Add files via upload

---
+++ Day 91/FindJudge.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        
+        int[] a = new int[N+1];
+		int[] b = new int[N+1];
+	    
+        for(int n:a) {
+            n = 0;
+        }
+        for(int n:b) {
+            n = 0;
+        }
+
+		for(int i=0; i<trust.length; i++) {
+            
+			a[trust[i][0]]++;
+			b[trust[i][1]]++;
+		}
+		for(int i=1; i<=N; i++) {
+            
+			if(a[i]==0 && b[i]==N-1) {
+				return i;
+            }
+		}
+		return -1;
+    }
+}
