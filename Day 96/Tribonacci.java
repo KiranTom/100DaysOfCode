060936a608f09039c5e319db3102aaf7a2322097
KiranTom <59497459+KiranTom@users.noreply.github.com>
2020-05-20 12:50:31 +0530

    Add files via upload

---
+++ Day 96/Tribonacci.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int tribonacci(int n) {
+        
+        if(n == 0) {
+            return 0;
+        }
+        if(n==1 || n==2) {
+            return 1;
+        }
+        else {
+            return calculate(n-2, 0, 1, 1);
+        }
+    }
+    
+    public int calculate(int n, int t0, int t1, int t2) {
+
+        int result = t2;
+        if(n == 0) {
+            return result;
+        }
+        else {
+            result = t0+t1+t2;
+            return calculate(n - 1 , t1, t2, result);
+        }
+    }
+}
